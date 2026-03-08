e321e5d7bd766d602e8f957ac368e11d6406d613
LUCKWIN SUBINAYA <luckwinsubinaya1@gmail.com>
2026-03-08 17:45:29 +0530

    Fix build

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from "react";
-import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
 
 import Navbar from "@/layout/navbar";
 import Footer from "@/layout/footer";
 import { routes } from "@/data/navigationRoutes";
 import { classNames } from "@/utility/classNames";
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
+const montserrat = localFont({
+  src: [
+    {
+      path: "../../public/fonts/static/Montserrat-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/static/Montserrat-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  display: "swap",
 });
 
 export interface MainLayoutProps {
@@ -16,16 +28,12 @@ export interface MainLayoutProps {
 
 export default function MainLayout(props: MainLayoutProps) {
   return (
-    //<html lang="en" className="dark" style={{ backgroundColor: "#000000" }}>
     <div className="grid-bg min-h-screen">
       <div className={classNames("min-h-screen", montserrat.className)}>
         <Navbar routes={routes} />
-
         <main>{props.children}</main>
-
         <Footer />
       </div>
     </div>
-    //</html>
   );
 }
