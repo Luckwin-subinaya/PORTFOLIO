5062abee54e10ffdb9cb170819c7bb6b4084ae81
LUCKWIN SUBINAYA <luckwinsubinaya1@gmail.com>
2026-03-08 18:47:36 +0530

    logo fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="dark">
-      <Head />
+      <Head>
+        <link rel="icon" href="/static/logo.png" />
+        <link rel="apple-touch-icon" href="/static/logo.png" />
+      </Head>
       <body className="bg-background text-white antialiased selection:bg-accent selection:text-background">
         <Main />
         <NextScript />
